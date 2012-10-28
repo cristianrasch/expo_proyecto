acde8f726b1c9b9cef58c6975eece317fabc4b1e
Cristian Rasch <cristianrasch@gmail.com>
2012-10-28 20:20:54 -0300

    Tidy up seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -11,4 +13,6 @@ exposition = FactoryGirl.create(:exposition, year: Date.today.year)
 project = FactoryGirl.create(:project, authors_count: 2, 
                               title: "QA Project", description: "Dummy test project", 
                               exposition: exposition)
-FactoryGirl.create_list :activity, 2, exposition: exposition
+FactoryGirl.create :activity, title: "Lucha de robots", exposition: exposition
+FactoryGirl.create :activity, title: "Museo de electrónica", exposition: exposition
+FactoryGirl.create :user, :confirmed, email: "admin@example.com", password: "secret", admin: true
